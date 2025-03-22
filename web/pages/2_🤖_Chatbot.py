f56b60397d408702125d55e6393b7f6ed7c797c6
Nuyoahwjl <3508529368@qq.com>
2025-03-23 00:45:41 +0800

    create Chatbot

---
+++ web/pages/2_🤖_Chatbot.py
@@ -0,0 +1,10 @@
+import streamlit as st
+ 
+st.set_page_config(
+    page_title="Hospitals",
+    page_icon="🏥",
+)
+
+st.chat_input("Please enter your question")
+st.chat_button("Send")
+st.chat_message("Hello! How can I help you today?")
