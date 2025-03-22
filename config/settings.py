c73db007dc152f92cbff09f3b15e8094e13a942c
Nuyoahwjl <3508529368@qq.com>
2025-03-22 17:20:00 +0800

    update

--- config/settings.py
+++ config/settings.py
@@ -5,7 +5,8 @@ from dotenv import load_dotenv
 load_dotenv('.env')
 
 class Config:
-    DEEPSEEK_API_KEY = os.getenv("DEEPSEEK_API_KEY")
+    # DEEPSEEK_API_KEY = os.getenv("DEEPSEEK_API_KEY")
+    DEEPSEEK_API_KEY = "sk-ffc5fcb4ba8e4f96a646753274c0c0c4"
     VECTORSTORE1_PATH = "/vectordb/vector_db_1"
     VECTORSTORE2_PATH = "/vectordb/vector_db_2"
     DATA = {
