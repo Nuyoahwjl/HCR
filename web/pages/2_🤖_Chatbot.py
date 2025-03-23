0249c069dd802b3b5ab16ae858268053b52d90da
Nuyoahwjl <3508529368@qq.com>
2025-03-23 23:59:22 +0800

    update Chatbot

--- web/pages/2_🤖_Chatbot.py
+++ web/pages/2_🤖_Chatbot.py
@@ -1,9 +1,12 @@
 import streamlit as st
- 
+import numpy as np
+
 st.set_page_config(
     page_title="Chatbot",
     page_icon="🤖",
 )
 
-st.chat_input("Please enter your question")
-st.chat_message("Hello! How can I help you today?")
+st.chat_input("assistant")
+with st.chat_message("Hello! How can I help you today?"):
+    st.write("Hello! How can I help you today?")
+    st.bar_chart(np.random.randn(30, 3))
