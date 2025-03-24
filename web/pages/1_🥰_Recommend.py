1cc54fb99692f4a8bc6e5a7cd1a6fbbf7ca4342a
Nuyoahwjl <3508529368@qq.com>
2025-03-24 20:10:04 +0800

    update font

--- web/pages/1_🥰_Recommend.py
+++ web/pages/1_🥰_Recommend.py
@@ -38,12 +38,12 @@ re = RecommendationChain()
 
 st.markdown("""
 <div style="text-align: center;">
-    <h2>🩺Health Check Recommendation</h2>
+    <h2><b>🩺Health Check Recommendation🩺</b></h2>
 </div>
 """, unsafe_allow_html=True)
 st.markdown("""
 <div style="text-align: center;">
-    <img src="https://readme-typing-svg.demolab.com?font=Fira+Code&weight=450&pause=1000&color=FF4B4BFF&center=true&vCenter=true&width=800&lines=Hi+there+👋,+Welcome+to+HCR!;Fill+in+the+following+information+to+get+the+recommendation." 
+    <img src="https://readme-typing-svg.demolab.com?font=Fira+Code&weight=550&pause=1000&color=FF4B4BFF&center=true&vCenter=true&width=800&lines=Hi+there+👋,+Welcome+to+HCR!;Fill+in+the+following+information+to+get+the+recommendation." 
          style="display: block; margin: auto; width: 100%;">
 </div>
 """, unsafe_allow_html=True)
