c6c4a73f71adc882982750dd8b0cc1f014088f4b
Nuyoahwjl <3508529368@qq.com>
2025-03-24 20:05:09 +0800

    update typing

--- web/pages/1_🥰_Recommend.py
+++ web/pages/1_🥰_Recommend.py
@@ -43,8 +43,8 @@ st.markdown("""
 """, unsafe_allow_html=True)
 st.markdown("""
 <div style="text-align: center;">
-    <img src="https://readme-typing-svg.demolab.com?font=Fira+Code&size=30&weight=450&pause=1000&color=FF4B4BFF&center=true&vCenter=true&width=800&lines=Hi+there+👋,+Welcome+to+HCR!;Fill+in+the+following+information+to+get+the+recommendation." 
-         style="display: block; margin: auto; width: 100%; max-width: 800px;">
+    <img src="https://readme-typing-svg.demolab.com?font=Fira+Code&weight=450&pause=1000&color=FF4B4BFF&center=true&vCenter=true&width=800&lines=Hi+there+👋,+Welcome+to+HCR!;Fill+in+the+following+information+to+get+the+recommendation." 
+         style="display: block; margin: auto; width: 100%;">
 </div>
 """, unsafe_allow_html=True)
 
