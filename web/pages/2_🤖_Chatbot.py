ee3c5ddfbcbee847f51a47149b9d6f7c3099d3f7
Nuyoahwjl <3508529368@qq.com>
2025-03-28 18:38:19 +0800

    update Chatbot

--- web/pages/2_🤖_Chatbot.py
+++ web/pages/2_🤖_Chatbot.py
@@ -7,6 +7,6 @@ st.set_page_config(
 )
 
 st.chat_input("Please type your message here...")
-# with st.chat_message("assistant"):
-#     st.write("Hello! How can I help you today?")
-#     st.bar_chart(np.random.randn(30, 3))
+with st.chat_message("assistant"):
+    st.write("Hello! How can I help you today?")
+    # st.bar_chart(np.random.randn(30, 3))
